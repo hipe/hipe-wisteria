b2ed3e3e3d7605f9ccf3d7df65e8a5cd7be45702
Mark Meves <mark.meves@gmail.com>
2010-01-09 16:02:38 -0500

    * draggable

--- js/default.js
+++ js/default.js
@@ -1,12 +1,8 @@
 jQuery(function(){
 
-  
-
- 
- 
- 
- 
- 
+  var gridw = Math.ceil($('#width-registration').width()/21)
+  var gridh = Math.ceil($('#width-registration').height()/2)
+  $("#square").draggable({ grid: [gridw,gridh], containment: 'parent'});
 
  
 });
