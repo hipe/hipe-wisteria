cfd1aaa410d574d754fa99b3de05cd166ceffff2
Mark Meves <mark.meves@gmail.com>
2010-01-09 16:32:53 -0500

    * about to see if we can make the table itself draggable and resizable

--- js/default.js
+++ js/default.js
@@ -3,6 +3,7 @@ jQuery(function(){
   var gridw = Math.ceil($('#width-registration').width()/21)
   var gridh = Math.ceil($('#width-registration').height()/2)
   $("#square").draggable({ grid: [gridw,gridh], containment: 'parent'});
-
+	$("#square").resizable({grid: [10,10]});
+	$("#resizable").resizable();
  
 });
