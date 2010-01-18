553a247df0da9937eec633192fae3cedeecd1f1a
Mark Meves <mark.meves@gmail.com>
2010-01-18 04:43:50 -0500

    * wrote 'visit' script (beginnings)

--- js/default.js
+++ js/default.js
@@ -4,7 +4,7 @@ jQuery(function(){
   var gridh = Math.ceil($('#width-registration-1').height()/2);
   $("#square-1").resizable_table({ grid: [gridw, gridh] });  /* must come before draggable.  why? */
   $("#square-1").draggable({ grid: [gridw,gridh], containment: 'parent'});
-  //$("#resizable-1").resizable( );
+  $("#resizable-1").resizable( );
   $('#canvas-1').arrows({grid: [gridw,gridh]});
   $('#canvas-1').canvas({grid: [gridw,gridh]});
 
