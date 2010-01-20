d418fe52ff1cbf613351ca9f6f0fb3b0da3d77b3
Mark Meves <mark.meves@gmail.com>
2010-01-20 09:22:44 -0500

    * improve dimension registration   * beginnings of visual test for scaling

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,68 @@
 (function($) {
 
-  var puts = window.console.log;
+  var puts = window.console ? window.console.log : function(m){};
 
-  $.widget("ui.canvas", {
+  var parsePx = function(str){
+    return parseFloat(/^-?(\d+(?:\.\d+)?)px$/.exec(str));
+  };
+  var pt = function(pt){ return "["+pt[0]+"]["+pt[1]+"]"; };
+
+  $.widget("ui.canvas_font_metrics", {
+    _init: function() {
+      window.fm = this; puts("window.fm set.");
+      this.recalculate() || puts(this.error());
+    },
+    error: function(msg){
+      if (msg) {
+        this.last_error_message = msg;
+        return false;
+      } else {
+        return "font_metrics fail: "+this.last_error_message;
+      }
+    },
+    recalculate: function(){
+      var w, h, tw, th, wf, hf, nh, nh2, nh3, t1;
+      t1 = (new Date).getTime();
+      w = this.element.width();
+      tw = this.element.parent().width();
+      wf = Math.floor(tw / w);
+      nh = this.element.html();
+      if (wf<1)
+        return this.error("registration text is wider than parent div");
+      if (wf!=1) {
+        nh = new Array(wf+1).join(nh);
+        this.element.html(nh);
+        w = this.element.width();
+        if (w>tw) return this.error("new width wider than target?");
+      }
+      w = parsePx(this.element.css('width'));
+      th = this.element.parent().height();
+      nh2 = "A<br/>B<br/>C<br/>D<br/>E<br/>F<br/>G<br/>"; // hard-coded 7 belo
+      this.element.html(nh2);
+      h = this.element.height();
+      if (h>th)
+        return this.error("registration height is greater than parent");
+      hf = Math.floor(th / h);
+      if (hf!=1) {
+        nh2 = new Array(hf+1).join(nh2);
+        this.element.html(nh2);
+        h = this.element.height();
+        if (h>th) return this.error("new height wider than target?");
+      }
+      var charsHigh = 7 * hf;
+      var charsWide = nh.length;
+      h = parsePx(this.element.css('height'));
+      var dim = [w/charsWide,h/charsHigh];
+      puts('fm dimensions w:'+w+" h:"+h+" dim:"+pt(dim)+
+        ' in '+((new Date).getTime() - t1)+' msec');
+      this.element.data('dimensions',dim);
+      this.element.html(nh+'<br/>'+nh2); //just for visual debugging
+      return true;
+    }
+  });
+
+
+  $.widget("ui.canvas_dots", {
     _init: function() {
       this.context = this.element[0].getContext("2d");
       this.context.fillStyle = this.element.css('color');
